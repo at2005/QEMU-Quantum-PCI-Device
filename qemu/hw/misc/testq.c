785f64ea1375bed1af5b06186245062c1c375547
atamb <tambdeayush@gmail.com>
2021-11-10 14:55:32 -0800

    implemented tensor products

--- qemu/hw/misc/testq.c
+++ qemu/hw/misc/testq.c
@@ -5,6 +5,7 @@
 #include "qc.h"
 
 #define CVAL(ADDR) (*(Complex*)(ADDR))
+#define CPTR(ADDR) ((Complex*)(ADDR))
 #define VVAL(ADDR) (*(CVec*)(ADDR))
 #define VPTR(ADDR) ((CVec*)(ADDR))
 
@@ -15,23 +16,27 @@ Complex conjugate(Complex a) {
 } 
 
 
-
+// addition
 Complex cadd(Complex a, Complex b) {
 	Complex sum = {a.real + b.real, a.imag + b.imag};
 	return sum;
 }
 
+
+// subtraction
 Complex csub(Complex a, Complex b) {
 	Complex diff = {a.real - b.real, a.imag - b.imag};
 	return diff;
 }
 
+// multiplication 
 Complex cmul(Complex a, Complex b) {
 	Complex prod = {a.real * b.real - (a.imag * b.imag), a.real * b.imag + (a.imag * b.real)};
 	return prod;
 }
 
 
+// division (multiplication by conjugate transpose)
 Complex cdiv(Complex a, Complex b) {
 	Complex ratio;
 	ratio.real = a.real * b.real + b.imag * a.imag;
@@ -42,11 +47,14 @@ Complex cdiv(Complex a, Complex b) {
 }
 
 
+// add element to vector
 void vec_push(CVec* vector, void* el) {
+	// allocate and assign new item
 	Item* new_item = (Item*)malloc(sizeof(Item));
 	new_item->val = el;
 	
 	
+	// check if vector size is zero so that we don't dereference any nullptr
 	if(vector->size != 0) {
 		vector->tail->next = new_item;
 		new_item->prev = vector->tail;
@@ -57,7 +65,7 @@ void vec_push(CVec* vector, void* el) {
 		return;
 	}
 	
-
+	// case for first element, set up vector head and tail
 	vector->head = new_item;
 	vector->tail = new_item;
 	new_item->prev = new_item;
@@ -67,8 +75,9 @@ void vec_push(CVec* vector, void* el) {
 }
 
 
-
+// create a vector
 CVec* create_vec() {
+	// allocate vector and initialize
 	CVec* new_vec = (CVec*)malloc(sizeof(CVec));
 	new_vec->head = (Item*)NULL;
 	new_vec->tail = (Item*)NULL;
@@ -79,6 +88,7 @@ CVec* create_vec() {
 }
 
 
+// get element at specified index
 Item* subscript(CVec* v, unsigned int index) {
 	Item* init = v->head;
 	for(int i = 0; i < index; i++) {
@@ -90,6 +100,7 @@ Item* subscript(CVec* v, unsigned int index) {
 
 }
 
+// get complex number at index
 Complex cindex(CVec* v, unsigned int index) {
 	Item* it = subscript(v, index);
 	return *(Complex*)(it->val);
@@ -97,10 +108,21 @@ Complex cindex(CVec* v, unsigned int index) {
 }
 
 
+// get vector at index (presumably in a matrix)
+CVec vindex(CVec* m, unsigned int index) {
+	Item* it = subscript(m,index);
+	return *(CVec*)(it->val);
+
+
+}
+
+
+// inner product (dot for short)
 Complex dot(CVec* a, CVec* b) {
 	Complex total = {0,0};
 	for(int i = 0; i < a->size; i++) {
-		total = cadd(total, cmul(CVAL(subscript(a,i)->val), CVAL(subscript(b,i)->val)));
+		// we take the conjugate of the bra because that is the definition of inner product
+		total = cadd(total, cmul(conjugate(CVAL(subscript(a,i)->val)), CVAL(subscript(b,i)->val)));
 	
 	}	
 	
@@ -108,6 +130,7 @@ Complex dot(CVec* a, CVec* b) {
 }
 
 
+// check if vectors are orthogonal
 int is_orthogonal(CVec* a, CVec* b) {
 	if(dot(a,b).real == 0 && dot(a,b).imag == 0) {
 		return 1;
@@ -119,28 +142,23 @@ int is_orthogonal(CVec* a, CVec* b) {
 }
 
 
-
+// print a formatted complex number
 void printc(Complex a) {
 	printf("%f+%fi\n", a.real, a.imag);
 
 }
 
 
-CVec vindex(CVec* m, unsigned int index) {
-	Item* it = subscript(m,index);
-	return *(CVec*)(it->val);
-
-
-}
-
-
-
+// apply a linear operation (usually matrix in a basis) to a vector
 CVec* linop(CVec* op, CVec* state) {
 	CVec* res = create_vec();
 	for(int i = 0; i < op->size; i++) {
+		// get row at specified index
 		CVec vec = vindex(op,i);
 		Complex* mul_res = (Complex*)malloc(sizeof(Complex));
+		// get the inner product of row + column vector
 		*mul_res = dot(&vec, state);
+		// append to result vector
 		vec_push(res, mul_res);
 	
 	}
@@ -161,6 +179,7 @@ Complex absq(Complex a) {
 }
 
 
+// print probabilities
 void print_prob(CVec* v) {
 	for(int i = 0; i < v->size; i++) {
 		printf("P(%d) is %f% \n", i, absq(CVAL(subscript(v,i)->val)).real * 100);
@@ -170,9 +189,116 @@ void print_prob(CVec* v) {
 
 }
 
+
+// print statevector
+void print_vector(CVec* v) {
+	for(int i = 0; i < v->size; i++) {
+		Complex num = CVAL(subscript(v,i)->val);
+		printf("(%f+%fi)|%d> ",num.real, num.imag,i);
+		if(i+1 < v->size) printf("+ ");
+	
+	}
+
+	printf("\n");
+
+
+
+}
+
+// get the transpose of a matrix (switch rows + columns)
+CVec* transpose(CVec * matrix) {
+	CVec* new_matrix = create_vec();
+	// iterate multiple times because we need to extract elements from the column vectors but 
+	// matrix is defined as a vector of row-vectors, so we have to iterate b^2 times where b is 
+	// the size of the matrix
+	for(int i = 0; i < matrix->size; i++) {
+		CVec* col = create_vec();
+		for(int j = 0; j < matrix->size; j++) {
+			// get the vector at index j and store in v
+			CVec* v = VPTR(subscript(matrix,j)->val);
+			// get the element at index i in v
+			Complex* c = CPTR(subscript(v,i)->val);
+			// append vector to matrix
+			vec_push(col,c);
+		
+		}
+
+		vec_push(new_matrix,col);	
+	
+	}
+
+	return new_matrix;
+
+} 
+
+
+ 
+// calculate tensor product for two vectors
+CVec* tensor_vec(CVec* a, CVec* b) {
+	CVec* new_vec = create_vec();
+	// iterate over each vector and each vector's elements and calculate tensor product
+	// e.g. (a_0*b_0, a_0*b_1, a_1*b0, a1*b1 ... a_n*b_n)
+	for(int i = 0; i < a->size; i++) {
+		Complex c1 = CVAL(subscript(a,i)->val);
+		for(int j = 0; j < b->size; j++) {
+			Complex c2 = CVAL(subscript(b,j)->val);
+			Complex* res = (Complex*)malloc(sizeof(Complex));
+			*res = cmul(c1,c2);
+			vec_push(new_vec, res);
+
+		}
+	
+	}	
+
+	return new_vec;
+}
+
+
+
+// calculate tensor for matrices
+CVec* tensor(CVec* a, CVec* b) {
+	CVec* tensor_prod = create_vec();
+	// transpose matrix b so that we can easily access columns as rows
+	CVec* b_dg = transpose(b);
+	// iterate and calculate vector tensor product and add to matrix
+	for(int i = 0; i < a->size; i++) {
+		CVec* col = VPTR(subscript(a,i)->val);
+		for(int j = 0; j < b_dg->size; j++) {
+			CVec* row = VPTR(subscript(b,j)->val);
+			vec_push(tensor_prod, tensor_vec(col,row)); 	
+
+		}
+	
+	}
+
+	for(int i = 0; i < b_dg->size; i++) {
+		free(VPTR(subscript(b_dg,i)->val));
+	}
+
+	free(b_dg);
+	
+	return tensor_prod;	
+}
+
+
+void print_matrix(CVec* m) {
+	for(int i = 0; i < m->size; i++) {
+		CVec* v = VPTR(subscript(m,i)->val);
+		for(int j = 0; j < v->size; j++) {
+			Complex c = CVAL(subscript(v,j)->val);
+			printf("%f + %fi  ", c.real, c.imag);
+			
+		}
+
+		printf("\n");
+	}
+
+}
+
+
 int main() {
-	Complex test1 = {sin(2),0};
-	Complex test2 = {cos(2),0};
+	Complex test1 = {1/sqrt(2),0};
+	Complex test2 = {1/sqrt(2),0};
 	//Complex test6 = {12,0};
 	
 	CVec* v = create_vec();
@@ -180,10 +306,10 @@ int main() {
 	vec_push(v,&test2);
 
 	
-	Complex m1 = {1/sqrt(2),0};
-	Complex m2 = {1/sqrt(2),0};
-	Complex m3 = {1/sqrt(2),0};
-	Complex m4 = {-1/sqrt(2),0};
+	Complex m1 = {1,0};
+	Complex m2 = {0,0};
+	Complex m3 = {0,0};
+	Complex m4 = {-1,0};
 	
 	CVec* c1 = create_vec();
 	
@@ -199,11 +325,31 @@ int main() {
 	vec_push(m, c1);
 	vec_push(m, c2);
 	
-	v = linop(m,v);
-	print_prob(v);
+	CVec* matrix2 = create_vec();
+	vec_push(matrix2, c1);
+	vec_push(matrix2, c2);
 
+	//CVec* tmat = transpose(m);
+	//print_matrix(tensor(m, matrix2));
+	
+	CVec* tmat = tensor(m,matrix2);
+	
+	CVec* vec4 = create_vec();
+	vec_push(vec4, &m2);
+	vec_push(vec4, &m2);
+	vec_push(vec4, &m1);
+	vec_push(vec4, &m2);
+	
+	vec4 = linop(tmat,vec4);
+		
+	print_vector(linop(tmat, vec4));
+
+//	v = linop(m,v);
+//	print_vector(v);
 	v = linop(m,v);
-	print_prob(v);
+	v = linop(m,v);
+
+	//	print_vector(v);
 	
 
 	return 0;
